eeddba0630558f4f2c8e9a5d781dbdc7a5080405
455016636 <455016636@qq.com>
2017-10-20 21:25:15 +0800

    Change_2

--- calculator_2.py
+++ calculator_2.py
@@ -14,7 +14,7 @@ def calulator(dic):
 	ls_tax = [1500, 4500, 9000, 35000, 55000, 80000]
 	ls_rate = [0.03, 0.1, 0.2, 0.25, 0.3, 0.35, 0.45]
 	ls_deduct = [0, 105, 555, 1005, 2755, 5505, 13505]
-	for value in dic.values():
+	for key, value in dic.items():
 		n = 0
 		if (value - 3500 - value * 0.165) < 0:
 			salary = value * (1 - 0.165)
